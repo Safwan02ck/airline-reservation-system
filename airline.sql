ba6a315d6fe20790ebd25a6953fa5b7bb6bb50b2
Safwan02ck <safwankamaal@gmail.com>
2025-06-05 10:42:16 +0530

    Update airline.sql

--- airline.sql
+++ airline.sql
@@ -3,12 +3,12 @@ USE airline;
 
 CREATE TABLE IF NOT EXISTS flights (
   flight_id INT NOT NULL AUTO_INCREMENT,
-  airline VARCHAR(255),
-  origin VARCHAR(255),
-  destination VARCHAR(255),
-  departure_time DATETIME,
-  arrival_time DATETIME,
-  price DECIMAL(10,2),
+  airline VARCHAR(255) DEFAULT NULL,
+  origin VARCHAR(255) DEFAULT NULL,
+  destination VARCHAR(255) DEFAULT NULL,
+  departure_time DATETIME DEFAULT NULL,
+  arrival_time DATETIME DEFAULT NULL,
+  price DECIMAL(10,2) DEFAULT NULL,
   PRIMARY KEY (flight_id)
 );
 
@@ -18,23 +18,33 @@ INSERT INTO flights (airline, origin, destination, departure_time, arrival_time,
 
 CREATE TABLE IF NOT EXISTS reservations (
   reservation_id INT NOT NULL AUTO_INCREMENT,
-  flight_id INT,
-  customer_name VARCHAR(255),
-  seat_number VARCHAR(5),
+  flight_id INT DEFAULT NULL,
+  customer_name VARCHAR(255) DEFAULT NULL,
+  seat_number VARCHAR(5) DEFAULT NULL,
   reservation_time DATETIME DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (reservation_id),
   FOREIGN KEY (flight_id) REFERENCES flights(flight_id)
-);
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4_0900_ai_ci;
 
+-- sample reservation (use actual flight_id valures from python or SELECT query)
+-- if your're not sure what flight_id is, you can select them first  
+INSERT INTO reservation (flight_id, custormer_name, seat_number, reservation_time) VALUES 
+  (1,'Jonh Doe','A1','2024-01-16 05:21:58')
+  (2,'Jane Smith','B2','2024-01-16 05:21:58')
+  (1,'Jonh Doe','A1','2024-01-16 05:22:58')
+  (1,'Jane Smith','B2','2024-01-16 05:22:58')
+  (1,'Mike Brown','C3','2024-01-16 05:23:30')
+  (1, 'Vaibhav Srivastava','A1',NULL)
+  -- Create the seats table
 CREATE TABLE IF NOT EXISTS seats (
   seat_id INT NOT NULL AUTO_INCREMENT,
-  flight_id INT,
-  seat_number VARCHAR(5),
-  status ENUM('available','reserved','sold'),
+  flight_id INT DEFAULT NULL,
+  seat_number VARCHAR(5) DEFAULT NULL,
+  status ENUM('available','reserved','sold') DEFAULT NULL,
   PRIMARY KEY (seat_id),
-  FOREIGN KEY (flight_id) REFERENCES flights(flight_id)
-);
-
+  KEY flight_id (flight_id)
+) ENGINE = InnoDb DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+--Insert seats (link them to the correct flight_id)
 INSERT INTO seats (flight_id, seat_number, status) VALUES
 (1, 'A1', 'reserved'), (1, 'A2', 'available'), (1, 'A3', 'available'),
 (1, 'B1', 'available'), (1, 'B2', 'available'), (1, 'B3', 'available'),
